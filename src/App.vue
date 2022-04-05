734a91db2d4c7f579e211fda25c5558b39b9ea68
ReoYabiku <alcremikyu.z@gmail.com>
2022-04-05 11:58:25 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <header class="header-logo">
+      Component
+    </header>
+    <div class="boxes">
+      <NameAndAge v-for="item in items" v-bind:key="item.id" v-bind:name="item.name" v-bind:age="item.age"></NameAndAge>
+    </div>
+    <footer class="footer-logo">
+      fever time 6th
+    </footer>
+    <hr width=95% align="right">
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NameAndAge from './components/NameAndAge.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    NameAndAge
+  },
+  data () {
+    return{
+      items:[
+        {
+          id: 1,
+          name: "aaa",
+          age: 123
+        },
+        {
+          id: 2,
+          name: "bbb",
+          age: 45
+        },
+        {
+          id: 3,
+          name: "ccc",
+          age: 78
+        }
+      ]
+    }
 }
+}
+
+
 </script>
 
 <style>
@@ -19,8 +53,32 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.header-logo{
+  font-size: 50px;
+  color: #999;
+  font-weight: bold;
+}
+
+.footer-logo{
+  font-size: 20px;
+  color: black;
+  text-align: right;
+  padding: 0 20px;
+  margin-bottom: -8px;
+}
+
+.boxes{
+  display: flex;
+  text-align: center;
+  justify-content: center;
+}
+
+hr{
+  text-align: right;
+  margin-right: 0;
+  margin-bottom: 40px;
 }
 </style>
---
+++ src/components/NameAndAge.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="box">
+        <h1>{{name}}さん</h1>
+        <h2>{{age}}歳</h2>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'NameAndAge',
+  props: {
+      name: String,
+      age: String
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: black;
+}
+
+.box{
+    border: 5px dotted black;
+    padding: 0 50px;
+    margin: 50px 10px;
+}
+
+</style>
